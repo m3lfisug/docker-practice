ff644ae746387b492d503cf09d7c2c737be86bda
福井雅浩 <>
2020-05-11 17:18:14 +0900

    レイアウト調整 共通するviewの部分を分ける

---
+++ docker-sample-2/src/resources/views/layouts/header.blade.php
@@ -0,0 +1,6 @@
+<header class="topNavigation">
+    <hr>
+    <p>header</p>
+    <p>selectItem : {{ $selectItem }}</p>
+    <hr>
+</header>
--- docker-sample-2/src/resources/views/student/index.blade.php
+++ docker-sample-2/src/resources/views/student/index.blade.php
@@ -1,27 +1,22 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <title>生徒一覧</title>
-    <link rel="stylesheet" href="css/topNavigation.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
-<header class="topNavigation">
-    <p>ナビゲーション</p>
-</header>
-<main>
-    <p>bladeの中身だよ</p>
+@extends('layouts.master')
+
+<!-- sectionの第一引数を受け取ったyieldに第二引数が反映される -->
+@section('title','生徒一覧')
+
+@section('content')
+    <hr>
+    <p>本文のcontent</p>
+    <p>bladeの中身</p>
     <p>このページには{{ $students->count() }}件表示中</p>
-    <table border="1" width="500" cellspacing="0" cellpadding="5" bordercolor="#333333">
+    <table border="1" width="500" cellspacing="0" bordercolor="#333333">
         <tr>
-            <th bgcolor="#EE0000">id</th>
-            <th bgcolor="#EE0000" width="150">名前</th>
-            <th bgcolor="#EE0000" width="200">年齢</th>
-            <th bgcolor="#EE0000" width="200">作成日</th>
-            <th bgcolor="#EE0000" width="200">更新日</th>
+            <th bgcolor="#b0c4de">id</th>
+            <th bgcolor="#b0c4de" width="150">名前</th>
+            <th bgcolor="#b0c4de" width="200">年齢</th>
+            <th bgcolor="#b0c4de" width="200">作成日</th>
+            <th bgcolor="#b0c4de" width="200">更新日</th>
         </tr>
-        @foreach($students AS $student)
+        @foreach($students as $student)
             <tr>
                 <td>{{ $student->id }}</td>
                 <td>{{ $student->name }}</td>
@@ -30,13 +25,12 @@
                 <td>{{ $student->updated_at }}</td>
             </tr>
         @endforeach
+        <!-- 結果の残りページ -->
         {{ $students->links() }}
     </table>
-</main>
-<footer>
-</footer>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
+
+@section('content_script')
+    <script>
+        console.log('script test student index');
+    </script>
