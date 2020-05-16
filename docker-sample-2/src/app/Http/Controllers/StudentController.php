8f410c870b44bd69d0fa55c83e114caa7008cced
福井雅浩 <>
2020-05-16 14:18:53 +0900

    名前と年齢のバリデーション、入力欄の値保持

--- docker-sample-2/src/app/Http/Controllers/StudentController.php
+++ docker-sample-2/src/app/Http/Controllers/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -50,13 +51,40 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        $student = new Student();
-        $student->name = $request->get('name');
-        $student->age = $request->get('age');
-        $student->save();
-        // with sessionに格納、一度表示したら消える
-        return redirect()->route('student.index')->with('success',$student->name . 'を追加しました');
+        // 入力条件（入力必須|型|文字数）
+        $validationRules = [
+            'name' => 'required|string|max:10',
+            'age' => 'required|integer|digits_between1,3',
+        ];
+        // 入力不十分の時に出る文言
+        $validatonMessages = [
+            'name.required' => '名前を入力してください',
+            'name.max' => '名前は10文字以下で入力してください',
+            'age.*' => '年齢は3桁以内で必ず入力してください'
+        ];
+        // $request->validate($validationRules,$validatonMessages);
+
+        $validator = Validator::make($request->all(),$validationRules,$validatonMessages);
+        if (!$validator->passes()) { //passes trueかfalseを返す
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        try {
+            $student = new Student();
+            $student->name = $request->get('name');
+            $student->age = $request->get('age');
+            $student->save();
+            Log::info('student created user id : '.$student->id);
+            // with sessionに格納、一度表示したら消える
+            return redirect()->route('student.index')->with('success',$student->name . 'を追加しました');
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+            return back()
+                ->withErrors(['システムエラー'])
+                ->withInput();
+        }
     }
 
     /**
